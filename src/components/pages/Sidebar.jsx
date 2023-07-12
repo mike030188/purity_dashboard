4a191b40d5105e5e6aa7c7874aca8456923206ad
mike030188 <halol.imnida2023@gmail.com>
2023-07-13 00:49:09 +0500

    feat: SideBar hover style has been done

--- src/components/pages/Sidebar.jsx
+++ src/components/pages/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BsHouseDoor, BsFillBarChartFill, BsCreditCard2BackFill, BsFillPersonFill} from 'react-icons/bs';
-import {FaWrench, FaFile, FaRocket} from 'react-icons/fa';
-import { AccPages, Container, Dashboard, Documentation, Items, Logo, LogoWrapper } from '../styles/Sidebar';
+import {FaWrench, FaFile, FaRocket, FaQuestionCircle} from 'react-icons/fa';
+import { AccPages, Button, Container, Dashboard, Documentation, Items, Logo, LogoWrapper } from '../styles/Sidebar';
 import logo from '../../assets/images/logo.png';
 import sidebarvector from '../../assets/images/SidebarVector.png';
 
@@ -13,7 +13,7 @@ const Sidebar = () => {
             <Logo src={logo} alt=""/>
             <h1>PURITY UI DASHBOARD</h1>
         </LogoWrapper>
-        <img src={sidebarvector} alt="line" />
+        <img src={sidebarvector} alt="line" style={{margin: '27.5px 0 22.5px'}}/>
         <Dashboard>
             <Items style={{color:'#2D3748'}}> <BsHouseDoor/>Dashboard</Items>
             <Items><BsFillBarChartFill/>Tables</Items>
@@ -27,7 +27,10 @@ const Sidebar = () => {
             <Items><FaRocket/>Sign Up</Items>
         </AccPages>
         <Documentation>
-            
+            <FaQuestionCircle style={{margin:'16px 0 21.5px'}}/>
+            <h3>Need help?</h3>
+            <p>Please check our docs</p>
+            <Button>DOCUMENTATION</Button>
         </Documentation>
     </Container>
   )
--- src/components/styles/Sidebar.js
+++ src/components/styles/Sidebar.js
@@ -8,13 +8,12 @@ export const Container = styled.div`
     height:100vh;
     border: 1px solid red;
     padding:38px 16px;
+    line-height: 150%;
 
 h1{
     color: #2D3748;    
     font-size: 14px;    
-    font-weight: 700;
-    line-height: 150%;
-    
+    font-weight: 700;      
 }   
 `
 
@@ -37,8 +36,16 @@ export const Dashboard = styled.div`
 export const Items = styled.div`
     color: var(--gray-gray-400, #A0AEC0);    
     font-size: 12px;    
-    font-weight: 700;
-    line-height: 150%;
+    font-weight: 700;  
+    cursor: pointer;
+
+    &:hover{
+        border:1px solid #FFF;
+        width: 219.5px;
+        height: 54px;
+        border-radius: 12px;
+        background-color: #FFF;
+    }
 `
 
 export const AccPages = styled.div`
@@ -50,5 +57,45 @@ export const Documentation = styled.div`
     background-repeat: no-repeat;
     height:170px;
     width:218px;
+    padding-left: 16px;
+    color: var(--black-amp-white-white, #FFF);
     
+h3{        
+    font-size: 14px;    
+    font-weight: 700;
+    line-height: 140%;
+    cursor: pointer;
+    &:hover{
+        text-decoration: underline;
+        color: green;
+    }
+}
+
+p{
+    margin-bottom: 47px;
+    font-size: 12px;    
+    font-weight: 400;   
+    color: var(--black-amp-white-white, #FFF); 
+}
+`
+export const Button = styled.button`
+    display:flex;
+    width:186px;
+    height:37px;
+    border-radius:12px;
+    border: none;
+    padding: 0px 8px;
+    justify-content: center;
+    align-items: center;
+    background: var(--black-amp-white-white, #FFF);
+    color: var(--gray-gray-700, #2D3748);
+    text-align: center;
+    font-size: 10px;
+    font-weight: 700;
+    cursor: pointer;
+
+&:hover{
+    background-color: violet;
+    color: var(--black-amp-white-white, #FFF);
+}
 `
